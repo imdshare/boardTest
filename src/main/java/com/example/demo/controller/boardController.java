0e20005e2f04db0806ba02386d5ad2d4fe5070f6
seungmilee <seungmilee@imdglobals.com>
2020-11-30 17:03:46 +0900

    ..

--- src/main/java/com/example/demo/controller/boardController.java
+++ src/main/java/com/example/demo/controller/boardController.java
@@ -2,13 +2,18 @@ package com.example.demo.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.demo.entity.NoticeEntity;
 import com.example.demo.service.NoticeService;
+
 
 @Controller
 public class boardController {
@@ -18,11 +23,36 @@ public class boardController {
 	
 	
 	@RequestMapping("/")
-	public String homem(Model model) {
-	List<NoticeEntity> list	= noticeService.noticelist();
-	model.addAttribute("list",list);
-	
+	public String home(Model model) {
+		List<NoticeEntity> list = noticeService.noticelist();
+		model.addAttribute("list", list);
+
 		return "index";
 	}
 	
+	@RequestMapping("/test")
+	public String test(Model model) {
+		List<NoticeEntity> list = noticeService.noticelist();
+		model.addAttribute("list", list);
+		
+		return "index";
+	}
+//	@GetMapping
+//	public String home(Model model) {
+//	List <NoticeEntity> list=noticeService.noticelist();
+//	if(list != null)
+//	model.addAttribute("list",list);
+//
+//	return "index";
+//	}
+	
+	
+//	private static final Logger logger = LoggerFactory.getLogger(boardController.class);
+//	
+//	@RequestMapping(value = "/", method = RequestMethod.GET)
+//	public String home() {
+//		logger.info("Welcome home!");
+//		
+//		return "index";
+//	}
 }
--- src/main/java/com/example/demo/entity/NoticeEntity.java
+++ src/main/java/com/example/demo/entity/NoticeEntity.java
@@ -1,36 +1,34 @@
 package com.example.demo.entity;
 
-import java.time.LocalDateTime;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
-import org.springframework.data.annotation.CreatedDate;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 
 @Data
 @Entity
+@Table(name="notice")
 public class NoticeEntity {
 	
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private String idx ;
-
-	@Column(name="noticeNo")
-	private String noticeNo ;
+	private long idx ;
 
 	@Column(name="title")
 	private String title ;
 
 	@Column(name="contents")
 	private long contents ;
-
-	@CreatedDate
-	private LocalDateTime regDt ;
+	
+	@Column(name="writer")
+	private String writer ;
+	
+	@Column(name="regDt")
+	private String regDt ;
 }
